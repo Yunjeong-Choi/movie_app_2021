11bc37a32514faa69b7a224dcc7acc2c4f61f63b
Yunjeong Choi <yunjeongchoi@Yunjeongui-MacBookAir.local>
2021-08-06 11:43:11 +0900

    practice map, propTypes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const listOfFood = [
+  {
+    name: 'kimchi',
+    image: ""
+  },
+  {
+    name: 'ramen',
+    image: ""
+  },
+  {
+    name: 'ggochi',
+    image: ""
+  }
+]
+
+function Food({name, img}) {
+  return (
+  <div>
+      <h2>I like {name}</h2>
+      <img src={img} alt={name}></img>
+  </div>
+  )
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {listOfFood.map(food => {
+        return(<Food
+          key={food.name}
+          name={food.name}
+          img={food.image}
+        />
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
